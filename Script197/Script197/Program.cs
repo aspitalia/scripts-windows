27921023a6cea7a19f32b041a7e856dc85b3c775
Matteo Tumiati <matteot@icubed.it>
2018-05-20 13:11:21 +0200

    Add script 197
    UWP console app

---
+++ Script197/Script197/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Script197
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Benvenuto nella UWP console app!");
+            var parameters = string.Join(Environment.NewLine, args);
+
+            if (string.IsNullOrEmpty(parameters))
+                Console.WriteLine("App invocata senza parametri");
+            else
+                Console.WriteLine($"App invocata con i parametri: {Environment.NewLine}{parameters}");
+
+            Console.ReadKey();
+        }
+    }
+}
